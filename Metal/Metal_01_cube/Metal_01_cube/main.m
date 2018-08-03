a75b5b9b986fbfa36baad395a9ef639c20df0356
ZHK1024 <389914070@qq.com>
2018-08-03 11:43:18 +0800

    Metal 绘制立方体

---
+++ Metal/Metal_01_cube/Metal_01_cube/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Metal_01_cube
+//
+//  Created by ZHK on 2018/8/3.
+//  Copyright © 2018年 ZHK. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
